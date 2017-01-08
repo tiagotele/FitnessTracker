7ca8cc68ddaaaf101d0ad38ced31ea7b8f87cbcf
Tiago Melo <tiagotele@gmail.com>
2017-01-08 19:07:52 -0300

    Return list as JSON for activities.

--- srs/main/java/com/pluralsight/controller/MinutesController.java
+++ srs/main/java/com/pluralsight/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package com.pluralsight.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.pluralsight.model.Activity;
 import com.pluralsight.model.Exercise;
 
 @Controller
@@ -15,6 +21,26 @@ public class MinutesController {
 		System.out.println("exercise: " + exercise.getMinutes());
 		
 		return "addMinutes";
+	}
+	
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities(){
+		List<Activity> activities = new ArrayList<>();
+		
+		Activity run = new Activity();
+		run.setDesc("Run");
+		
+		Activity bike = new Activity();
+		bike.setDesc("Bike");
+		
+		Activity swim = new Activity();
+		swim.setDesc("Swim");
+
+		activities.add(run);
+		activities.add(bike);
+		activities.add(swim);
+		
+		return activities;
 	}
 
 }
